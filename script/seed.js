ccb20a8fc72a5a0ff1f302bdf81827d4ae6f8bb3
Natalia Gabuaeva <ngabuaeva@gmail.com>
2019-11-05 15:52:52 -0500

    seed success

--- script/seed.js
+++ script/seed.js
@@ -2,6 +2,46 @@
 
 const db = require('../server/db')
 const {User} = require('../server/db/models')
+const {Products} = require('../server/db/models')
+
+const products = [
+  {
+    name: 'zebra',
+    price: 5.0,
+    color: 'multi',
+    theme: 'animals',
+    description: 'zebra socks',
+    imageUrl:
+      'https://www.happysocks.com/media/catalog/product/h/t/httpsasset.productmarketingcloud.comapiassetstorage233_71669ae6-1eb1-4cc4-bc44-f67dac27e802magentoimage.png?quality=80&fit=bounds&height=700&width=700&canvas=700:700'
+  },
+  {
+    name: 'elephant',
+    price: 7.0,
+    color: 'multi',
+    theme: 'animals',
+    description: 'elephant socks',
+    imageUrl:
+      'https://www.happysocks.com/media/catalog/product/h/t/httpsasset.productmarketingcloud.comapiassetstorage233_3a97012d-e305-400f-954f-2cb15c442ab8magentoimage.png?quality=80&fit=bounds&height=700&width=700&canvas=700:700'
+  },
+  {
+    name: 'tulip',
+    price: 4.0,
+    color: 'multi',
+    theme: 'flowers',
+    description: 'tulip socks',
+    imageUrl:
+      'https://www.happysocks.com/media/catalog/product/h/t/httpsasset.productmarketingcloud.comapiassetstorage233_b52e0c67-7c37-4e30-b489-f00561272d84magentoimage.png?quality=80&fit=bounds&height=700&width=700&canvas=700:700'
+  },
+  {
+    name: 'cereal',
+    price: 8.0,
+    color: 'pink',
+    theme: 'food',
+    description: 'cereal socks',
+    imageUrl:
+      'https://www.happysocks.com/media/catalog/product/m/a/magentoimage_92.png?quality=80&fit=bounds&height=700&width=700&canvas=700:700'
+  }
+]
 
 async function seed() {
   await db.sync({force: true})
@@ -11,8 +51,13 @@ async function seed() {
     User.create({email: 'cody@email.com', password: '123'}),
     User.create({email: 'murphy@email.com', password: '123'})
   ])
-
+  const socks = await Promise.all(
+    products.map(product => {
+      return Products.create(product)
+    })
+  )
   console.log(`seeded ${users.length} users`)
+  console.log(`seeded ${socks.length} socks`)
   console.log(`seeded successfully`)
 }
 
