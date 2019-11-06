d40258c116db7ff63d9c435cc2cef709819514b9
Pauline Kong <pkong89@gmail.com>
2019-11-06 14:43:19 -0500

    updated reducer

--- client/store/cart.js
+++ client/store/cart.js
@@ -1,39 +1,91 @@
 import axios from 'axios'
-import {DELETE} from 'sequelize/types/lib/query-types'
 
 //ACTION TYPES
-const ALL_ITEMS = 'ALL_ITEMS'
-const ADD_ITEM = 'ADD_ITEM'
-const DELETE_ITEM = 'DELETE_ITEM'
+export const GET_CART_ITEMS = 'GET_CART_ITEMS'
+const UPDATE_CART = 'UPDATE_CART'
+const DELETE_FROM_CART = 'DELETE_FROM_CART'
 
 //ACTION CREATORS
-const allItems = items => ({
-  type: ALL_ITEMS,
+const getCartItems = items => ({
+  type: GET_CART_ITEMS,
   items
 })
 
-const addItem = item => ({
-  type: ADD_ITEM,
-  item
+const updateCart = cart => ({
+  type: UPDATE_CART,
+  cart
 })
 
-const deleteItem = item => ({
-  type: DELETE_ITEM,
-  item
+//delete specific item
+const deleteItem = productId => ({
+  type: DELETE_FROM_CART,
+  productId
 })
 
-// THUNKS
+//INITIAL STATE
+const initialState = {
+  cart: []
+}
 
-const initialState = []
+//THUNKS
 
+export const fetchCart = () => async dispatch => {
+  try {
+    const res = await axios.get('/api/cart')
+    dispatch(getCartItems(res.data))
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export const addItemToCart = cartItem => async dispatch => {
+  try {
+    const {data} = await axios.post(`/api/orders`, cartItem)
+    dispatch(updateCart(data))
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export const updateQuantity = (productId, quantity) => async dispatch => {
+  try {
+    const response = await axios.put(`/api/orders/cart/${productId}`, {
+      quantity: quantity
+    })
+    const updatedCart = response.data
+    dispatch(updateCart(updatedCart))
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export const removeFromCart = productId => async dispatch => {
+  try {
+    await axios.delete(`/api/orders/cart/${productId}`)
+    dispatch(deleteItem(productId))
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+//Reducer
 export default function(state = initialState, action) {
   switch (action.type) {
-    case ALL_ITEMS:
-      return action.items
-    case ADD_ITEM:
-      return action.item
-    case DELETE_ITEM:
-      return state.filter
+    case GET_CART_ITEMS: {
+      return {
+        ...state,
+        cart: action.items
+      }
+    }
+    case UPDATE_CART: {
+      return {...state, cart: action.cart}
+    }
+    case DELETE_FROM_CART: {
+      return {
+        ...state,
+        cart: state.cart.filter(input => input.productId !== action.productId)
+      }
+    }
     default:
       return state
   }
