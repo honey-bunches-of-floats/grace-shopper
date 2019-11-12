acbce9b01f17c0c542cb8f92206568f5f60d3080
Enya <enya.baez@gmail.com>
2019-11-12 16:02:08 -0500

    changed landing page

--- client/components/landingPage.js
+++ client/components/landingPage.js
@@ -2,9 +2,13 @@ import React from 'react'
 
 const Landing = () => {
   return (
-    <div className="jumbotron jumbotron-fluid">
-      <div className="container">
-        <h1 className="display-4">Welcome to the New York Sock Exchange</h1>
+    <div className="card bg-dark text-white">
+      <img
+        src="https://www.happysocks.com/media/wysiwyg/Grid-images/2019/FW19/Happy-holidays/Bestsellers_HappyHolidays_FrontPageGrid.jpg"
+        className="card-img"
+      />
+      <div className="card-img-overlay">
+        <h5 className="display-4">Welcome to the New York Sock Exchange</h5>
       </div>
     </div>
   )
